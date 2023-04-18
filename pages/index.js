e300ea4231c094f95567218aa4f253a97d6b577d
ore <go1ruf2tk3@gmail.com>
2023-04-18 10:13:22 +0900

    feat:fetch 영화 데이터

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 import Seo from "@/components/Seo";
 
-export default function index() {
+const API_KEY = "1ee59216974dff70e737a91025c747cc";
+
+export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
   );
 }
