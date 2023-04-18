6954b6e2cd9fc7886f4b751f34a09b8c108db2c6
ore <go1ruf2tk3@gmail.com>
2023-04-18 11:36:11 +0900

    feat: rewrite 사용해서 api key 숨기기

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,64 @@
-import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "@/components/Seo";
-
-const API_KEY = "1ee59216974dff70e737a91025c747cc";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [movies, setMovies] = useState();
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`);
+  };
 
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
+
   return (
-    <>
+    <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
+      {!movies && <div>Loading...</div>}
       {movies?.map((movie) => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              {movie.original_title}
+            </Link>
+          </h4>
         </div>
       ))}
-    </>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
 }
